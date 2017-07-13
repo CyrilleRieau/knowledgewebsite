05375e62662d4591beb796ef36effdfed331e400
Cyrille <cyrille.rieau@gmail.com>
2017-07-13 17:26:51 +0200

    Test Supression User

--- deleteUser.php
+++ deleteUser.php
@@ -4,13 +4,11 @@ session_start();
 
 if (isset($_POST['duser'])) {
     $y = unserialize(base64_decode($_POST['duser']));
-    
+    var_dump($y);
     foreach (Database::recupUser() as $user) {
-        var_dump($user);
         if ($y->getPseudo() !== $user->getPseudo()) {
             echo 'lol';
-    //Database::userCreate($user));
-    
+            unlink($y); //trouver méthode qui supprime l'objet; Genre faire un tableau des valeurs précédentes, dans le style modif
             //$_SESSION = [];
             //session_destroy();
         }
