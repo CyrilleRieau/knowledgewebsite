74208e4fb30bbb9d90123272ae9e347205fbc185
Cyrille <cyrille.rieau@gmail.com>
2017-07-18 14:48:19 +0200

    Ajout Table SQL

--- event_db.sql
+++ event_db.sql
@@ -11,32 +11,32 @@ CREATE TABLE `user` (
     avatar BLOB,
     age DATE NOT NULL,
     mail VARCHAR(64) NOT NULL,
-    password VARCHAR(64) NOT NULL,
+    password VARCHAR(1024) NOT NULL,
 );
 
 CREATE TABLE `post` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     contenu VARCHAR(1064) NOT NULL,
-    date DATE NOT NULL,
+    `date` DATE NOT NULL,
     auteur VARCHAR(64) NOT NULL,
     discipline VARCHAR(64) NOT NULL, 
     titre VARCHAR(64) NOT NULL,
     tags VARCHAR(1056) NOT NULL,
     upvotes INT,
-    downvotes INT 
+    downvotes INT,
+    `user`_id INT,
+    FOREIGN KEY (`user`_id) REFERENCES `user`(id)
 );
 
 CREATE TABLE `comment` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     contenu VARCHAR(1064) NOT NULL,
-    date DATE NOT NULL,
+    `date` DATE NOT NULL,
     auteur VARCHAR(64) NOT NULL,
     upvotes INT,
-    downvotes INT 
+    downvotes INT,
+    post_id INT,
+    `user`_id INT,
+    FOREIGN KEY (post_id) REFERENCES post(id),
+    FOREIGN KEY (`user`_id) REFERENCES `user`(id)
 );
-
-
-/* Créer les liens entre les tables
-CREATE TABLE album_member (album_id INT NOT NULL, member_id INT NOT NULL, 
-FOREIGN KEY (album_id) REFERENCES album(id), FOREIGN KEY (member_id) REFERENCES member(id),
-PRIMARY KEY (album_id, member_id));
